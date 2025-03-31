490127e8d349351bbdfac7d66b9e1bc8a797247b
Kelvin Ndunda <kelvinndunda.github@gmail.com>
2025-03-31 13:58:24 +0300

    finished assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+  function superbowlWin(object){
+      
+    const result=object.find((item,index)=>item.result=="W")
+    if(result){
+        return result.year
+    }else{
+        return undefined
+    }
+  }
+  superbowlWin(record)
